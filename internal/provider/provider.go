29ac3530e9f0c5fe2b1256633461a3220e50240b
alex massaroni <alex.m@seeweb.it>
2025-07-22 11:50:08 +0200

    Import template scaffolding

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,47 +1,101 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package provider
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
+	"github.com/hashicorp/terraform-plugin-framework/function"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-
-	"github.com/seeweb/seeweb-tf-provider/internal/data"
-	"github.com/seeweb/seeweb-tf-provider/internal/resource"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// New returns a new instance of the Seeweb provider.
-func New() provider.Provider {
-	return &seewebProvider{}
+// Ensure ScaffoldingProvider satisfies various provider interfaces.
+var _ provider.Provider = &ScaffoldingProvider{}
+var _ provider.ProviderWithFunctions = &ScaffoldingProvider{}
+var _ provider.ProviderWithEphemeralResources = &ScaffoldingProvider{}
+
+// ScaffoldingProvider defines the provider implementation.
+type ScaffoldingProvider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
 }
 
-type seewebProvider struct{}
-
-func (p *seewebProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "seeweb"
-	resp.Version = "0.1.0"
+// ScaffoldingProviderModel describes the provider data model.
+type ScaffoldingProviderModel struct {
+	Endpoint types.String `tfsdk:"endpoint"`
 }
 
-func (p *seewebProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
-	// No provider-level configuration yet.
+func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "scaffolding"
+	resp.Version = p.version
 }
 
-func (p *seewebProvider) Configure(_ context.Context, _ provider.ConfigureRequest, _ *provider.ConfigureResponse) {
-	// No-op configuration for now.
-}
-
-func (p *seewebProvider) Resources(_ context.Context) []func() provider.Resource {
-	return []func() provider.Resource{
-		func() provider.Resource { return &resource.DummyResource{} },
+func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"endpoint": schema.StringAttribute{
+				MarkdownDescription: "Example provider attribute",
+				Optional:            true,
+			},
+		},
 	}
 }
 
-func (p *seewebProvider) DataSources(_ context.Context) []func() provider.DataSource {
-	return []func() provider.DataSource{
-		func() provider.DataSource { return &data.DummyDataSource{} },
+func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data ScaffoldingProviderModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Configuration values are now available.
+	// if data.Endpoint.IsNull() { /* ... */ }
+
+	// Example client configuration for data sources and resources
+	client := http.DefaultClient
+	resp.DataSourceData = client
+	resp.ResourceData = client
+}
+
+func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewExampleResource,
 	}
 }
 
-var _ provider.Provider = (*seewebProvider)(nil)
+func (p *ScaffoldingProvider) EphemeralResources(ctx context.Context) []func() ephemeral.EphemeralResource {
+	return []func() ephemeral.EphemeralResource{
+		NewExampleEphemeralResource,
+	}
+}
 
-var _ providerserver.ProviderServer = &providerserver.SimpleProviderServer{Provider: New()}
+func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		NewExampleDataSource,
+	}
+}
+
+func (p *ScaffoldingProvider) Functions(ctx context.Context) []func() function.Function {
+	return []func() function.Function{
+		NewExampleFunction,
+	}
+}
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &ScaffoldingProvider{
+			version: version,
+		}
+	}
+}
